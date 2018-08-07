57fe2e383451b964bbcc6389a832886417e72ff2
markring <mark@boldtech.co>
2018-08-07 19:11:36 -0400

    jawsdb

--- api/v1/models/index.js
+++ api/v1/models/index.js
@@ -6,11 +6,13 @@ const env = process.env.NODE_ENV || 'development'
 const config = require(path.join(__dirname, '..','..','..', 'config/config.json'))[env]
 const db = {}
 
-let DB1 = process.env.SEQUEL_DB || config.database
-let userName = process.env.SEQUEL_USERNAME || config.username
-let password = process.env.SEQUEL_PASSWORD || config.password
 // TODO - Update for JawsDB on deployment
-let sequelize = new Sequelize(DB1, userName, password, config)
+if (process.env.NODE_ENV=='production' && process.env.JAWSDB_URL) {
+  let sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  let sequelize = new Sequelize(config.database, config.username, config.password, config)
+}
+
 
 fs
   .readdirSync(__dirname)
